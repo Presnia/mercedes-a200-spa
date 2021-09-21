fb8a930bda587b2a60012f690f9699f007dbdc42
Andrei Dzebysh <presnia@yahoo.com>
2021-09-21 14:06:44 +0300

    Full functionality of the accordion is implemented and adjusted styles

--- js/accordion.js
+++ js/accordion.js
@@ -3,14 +3,17 @@ const featureLists = document.querySelectorAll('.feature-sub');
 
 featureLinks.forEach((item, index) => {
   item.addEventListener('click', () => {
-    featureLinks.forEach((item) =>
-      item.classList.remove('feature__link_active')
-    );
+    if (featureLinks[index].classList.contains('feature__link_active')) {
+      featureLinks[index].classList.remove('feature__link_active');
+      featureLists[index].classList.add('hidden');
+    } else {
+      featureLinks.forEach((item) =>
+        item.classList.remove('feature__link_active')
+      );
+      featureLists.forEach((listItem) => listItem.classList.add('hidden'));
 
-    item.classList.add('feature__link_active');
-
-    featureLists.forEach((listItem) => listItem.classList.add('hidden'));
-
-    featureLists[index].classList.remove('hidden');
+      item.classList.add('feature__link_active');
+      featureLists[index].classList.remove('hidden');
+    }
   });
 });

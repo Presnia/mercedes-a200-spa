e13181e4921bc1b21ef9b0fd466879839a165891
Andrei Dzebysh <presnia@yahoo.com>
2021-09-21 12:08:07 +0300

    The accordion functionality is implemented

--- js/accordion.js
+++ js/accordion.js
@@ -1,1 +1,23 @@
-const featureLink = document.querySelectorAll('.feature__link');
+const featureLinks = document.querySelectorAll('.feature__link');
+const featureLists = document.querySelectorAll('.feature-sub');
+
+featureLinks.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    featureLinks.forEach((item) =>
+      item.classList.remove('feature__link_active')
+    );
+
+    item.classList.add('feature__link_active');
+
+    featureLists.forEach((listItem) => listItem.classList.add('hidden'));
+
+    featureLists[index].classList.remove('hidden');
+  });
+
+  if (item.classList.contains('feature__link_active')) {
+    item.addEventListener('click', () => {
+      item.classList.toggle('feature__link_active');
+      featureLists[index].classList.toggle('hidden');
+    });
+  }
+});
